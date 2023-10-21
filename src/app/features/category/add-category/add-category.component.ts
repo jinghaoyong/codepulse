e0c9c1fb2c79dc1276bde12b428bb983b93fbfa3
jinghaoyong <98522703+jinghaoyong@users.noreply.github.com>
2023-10-21 12:26:14 +0800

    jinghao push code at starbuck. Sunday. 21/10/2023

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -1,33 +1,43 @@
 import { CategoryService } from './../services/category.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
 
   model: AddCategoryRequest;
+  private addCategorySubscription?: Subscription;
 
-  constructor(private categoryServ: CategoryService) {
+  constructor(private categoryServ: CategoryService, private router: Router) {
     this.model = {
       name: '',
       urlHandle: ''
     };
   }
 
+
   onFormSubmit() {
     console.log('clicked on form submit')
-    this.categoryServ.addCategory(this.model).subscribe({
-      next:(res)=>{
+    this.addCategorySubscription = this.categoryServ.addCategory(this.model).subscribe({
+      next: (res) => {
+        this.router.navigateByUrl('/admin/categories');
         console.log("next : response", res)
 
       },
-      error:(res)=>{
+      error: (res) => {
         console.log("error : response", res)
       }
     })
   }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
+  }
+
 }
