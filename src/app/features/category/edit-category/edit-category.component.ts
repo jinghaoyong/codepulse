21dca53ec26dce748391d3504424ca2d1ec55657
Yong Jing Hao <jinghaoyong@pcomssb.com>
2023-11-27 21:07:16 +0800

    part 48 done

--- src/app/features/category/edit-category/edit-category.component.ts
+++ src/app/features/category/edit-category/edit-category.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-edit-category',
   templateUrl: './edit-category.component.html',
   styleUrls: ['./edit-category.component.css']
 })
-export class EditCategoryComponent {
+export class EditCategoryComponent implements OnInit, OnDestroy {
+
+  id: string | null = null;
+  paramsSubscription?: Subscription;
+  category?: Category;
+
+  constructor(
+    private route: ActivatedRoute,
+    private categoryServ: CategoryService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.paramsSubscription = this.route.paramMap.subscribe({
+      next: (params) => {
+        this.id = params.get('id');
+
+        if (this.id) {
+          this.categoryServ.getCategoryById(this.id).subscribe({
+            next: (res) => {
+              this.category = res;
+            }
+          })
+        }
+      }
+    })
+  }
+
+  onFormSubmit(): void {
+    console.log(this.category);
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription?.unsubscribe();
+  }
 
 }
