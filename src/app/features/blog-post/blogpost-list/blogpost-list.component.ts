c78eede59879a1da74b762e19f5c097c29e731c1
Yong Jing Hao <jinghaoyong@pcomssb.com>
2024-01-11 15:00:47 +0800

    done addblog post , markdown and image preview

--- src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogPostService } from '../services/blog-post.service';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post.model';
 
 @Component({
   selector: 'app-blogpost-list',
   templateUrl: './blogpost-list.component.html',
   styleUrls: ['./blogpost-list.component.css']
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit {
+
+  blogPost$?: Observable<BlogPost[]>;
+
+  constructor(
+    private blogPostServ : BlogPostService
+  ){
+
+  }
+
+  ngOnInit(): void {
+    // get all blogpost from api
+    this.blogPost$ =  this.blogPostServ.getAllBlogPosts();
+  }
 
 }
